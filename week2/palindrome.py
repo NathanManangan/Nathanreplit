3ddd0b275f22800e7d133a9e15339a6fa98667a5
ProRichyMan <89236893+ProRichyMan@users.noreply.github.com>
2022-03-30 20:38:53 +0000

    Recreated replit

---
+++ week2/palindrome.py
@@ -0,0 +1,19 @@
+weekimport re
+
+class Palindrome:
+  def __call__(self, n):
+    str(n)
+    reverse_n = n[::-1]
+      
+    new_n = (re.sub('[^a-zA-Z0-9]', '', n)).lower()
+    return new_n == new_n[::-1]
+
+def tester():
+  pal_of = Palindrome()
+  str_input = str(input("Enter a string or number (only accepts alphanumeric characters): "))
+  if pal_of(str_input):
+    print(str_input + " is a palindrome.")
+  elif pal_of(str_input) == False:
+    print(str_input + " is not a palindrome.")
+  else:
+    print("Oh no! Something went wrong!")
